ab5724deac867c986460f6f72933c14a3d393872
sdiazcor-max <sdiazcor@gmail.com>
2026-06-14 18:02:25 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+const FOGOFF_CONFIG = {
+  // API Keys
+  googleMapsKey: 'AIzaSyDnDhUKqbgONKUFbBi-t429bLIeKjpROD4',
+  web3formsKey: '5281736b-9f0f-49fe-8c70-d6d78c0cc8a0',
+  
+  // Stripe Payment Links
+  stripe: {
+    starterFog:  'https://buy.stripe.com/cNiaEW...',
+    fullSend:    'https://buy.stripe.com/8x24gy...',
+    fogBoss:     'https://buy.stripe.com/6oU4gy...',
+    oneTime:     'https://buy.stripe.com/9B66oG...',
+    eventFog:    'https://buy.stripe.com/9B6cN4...',
+  },
+
+  // Contact
+  phone:     '704-997-3336',
+  phoneE164: '+17049973336',
+  email:     'hello@fogoff.net',
+
+  // n8n webhook
+  n8nWebhook: 'https://hellofogoff.app.n8n.cloud/webhook/3fd3e1ca...',
+}
